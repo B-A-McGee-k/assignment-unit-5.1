85b318c3193f89bb4ae289faae58541223658220
B-A-McGee-k <mcgee.bryan.andrew@gmail.com>
2024-12-18 17:32:34 -0600

    Update object-practice.js
    Getting it up
    Known bug- Editing info function is incomplete

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,6 +9,11 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
+  firstName: 'Bryan',
+  lastName: 'McGee',
+  hasSiblings: true,
+  shoeCount: 8,
+  favThreeFoods: ['Ramen', 'Nachos', 'Cantaloupe'],
   // TODO - add properties here
 };
 console.log('A little about me:', me);
@@ -20,14 +25,19 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
-
+let fullName = me.firstName + ' ' + me.lastName
+console.log(fullName)
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
 
-
+console.log(me.favThreeFoods[0])
+console.log(me.favThreeFoods[2])
+//Bryan note:
+//Since the array is "3 favorite foods", would it be reasonable in this case to use 2?
+//The array is only going to have 3 items.
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -36,6 +46,25 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+console.log(me.shoeCount + ' shoes are what I currently have!');
+// Do not ask why I have that many, finding size 13 Vans at a reasonable price is hard so I buy what I can when I can :(
+
+let newValue = 0
+function changeProperty (me, info, change) {
+
+  if (info = 'shoeCount') {
+    console.log(info + change);
+    me.shoeCount = info + change
+    newValue = 'I have ' + me.shoecount + ' shoes'
+  }
+
+  //There would need to be another if and an else for any of the other info pieces to be changed
+  //Something for another
+  
+  return newValue + ' now!'
+}
+
+console.log(changeProperty(me, 'shoeCount', " + 1"))
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +72,8 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = "Cyan";
+console.log(me);
+
+
